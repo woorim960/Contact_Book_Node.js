08769fe80eedd919fe40e0e6c681e4092db504dd
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-03-02 04:19:27 +0900

    Create Contact-Book & Connect MongoDB

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var mongoose = require('mongoose')
+var app = express()
+
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.connect(process.env.MONGO_DB)
+var db = mongoose.connection
+
+db.once('open', function() {
+    console.log('DB connected')
+})
+
+db.on('error', function(err) {
+    console.err('DB ERROR : ', err)
+})
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+var port = 3000
+app.listen(port, function() {
+    console.log('server on! http://localhost/' + port)
+})
